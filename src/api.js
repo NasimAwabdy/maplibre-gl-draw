6396d3c3f5b1b0547893d583554110e9f2845362
NasimAwabdy <nasim.awabdy@anny.co>
2024-01-16 14:49:50 +0100

    Remove logic related to default package modes.

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import isEqual from 'lodash.isequal';
 import normalize from '@mapbox/geojson-normalize';
 import hat from 'hat';
 import featuresAt from './lib/features_at';
-import stringSetsAreEqual from './lib/string_sets_are_equal';
 import * as Constants from './constants';
 import StringSet from './lib/string_set';
 
@@ -126,46 +125,19 @@ export default function(ctx, api) {
 
   api.delete = function(featureIds) {
     ctx.store.delete(featureIds, { silent: true });
-    // If we were in direct select mode and our selected feature no longer exists
-    // (because it was deleted), we need to get out of that mode.
-    if (api.getMode() === Constants.modes.DIRECT_SELECT && !ctx.store.getSelectedIds().length) {
-      ctx.events.changeMode(Constants.modes.SIMPLE_SELECT, undefined, { silent: true });
-    } else {
-      ctx.store.render();
-    }
+    ctx.store.render();
 
     return api;
   };
 
   api.deleteAll = function() {
     ctx.store.delete(ctx.store.getAllIds(), { silent: true });
-    // If we were in direct select mode, now our selected feature no longer exists,
-    // so escape that mode.
-    if (api.getMode() === Constants.modes.DIRECT_SELECT) {
-      ctx.events.changeMode(Constants.modes.SIMPLE_SELECT, undefined, { silent: true });
-    } else {
-      ctx.store.render();
-    }
+    ctx.store.render();
 
     return api;
   };
 
   api.changeMode = function(mode, modeOptions = {}) {
-    // Avoid changing modes just to re-select what's already selected
-    if (mode === Constants.modes.SIMPLE_SELECT && api.getMode() === Constants.modes.SIMPLE_SELECT) {
-      if (stringSetsAreEqual((modeOptions.featureIds || []), ctx.store.getSelectedIds())) return api;
-      // And if we are changing the selection within simple_select mode, just change the selection,
-      // instead of stopping and re-starting the mode
-      ctx.store.setSelected(modeOptions.featureIds, { silent: true });
-      ctx.store.render();
-      return api;
-    }
-
-    if (mode === Constants.modes.DIRECT_SELECT && api.getMode() === Constants.modes.DIRECT_SELECT &&
-      modeOptions.featureId === ctx.store.getSelectedIds()[0]) {
-      return api;
-    }
-
     ctx.events.changeMode(mode, modeOptions, { silent: true });
     return api;
   };
