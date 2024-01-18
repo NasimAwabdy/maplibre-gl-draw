3596efd1414af6d74d20d8eeb650b614329aab9f
NasimAwabdy <nasim.awabdy@anny.co>
2024-01-18 15:09:40 +0100

    feat: remove setting cursor classes and updating map classes within the getFeatureAtAndSetCursors function.

--- src/lib/get_features_and_set_cursor.js
+++ src/lib/get_features_and_set_cursor.js
@@ -1,19 +1,6 @@
 import featuresAt from './features_at';
-import * as Constants from '../constants';
 
 export default function getFeatureAtAndSetCursors(event, ctx) {
   const features = featuresAt.click(event, null, ctx);
-  const classes = { mouse: Constants.cursors.NONE };
-
-  if (features[0]) {
-    classes.mouse = (features[0].properties.active === Constants.activeStates.ACTIVE) ?
-      Constants.cursors.MOVE : Constants.cursors.POINTER;
-    classes.feature = features[0].properties.meta;
-  }
-
-
-  ctx.ui.queueMapClasses(classes);
-  ctx.ui.updateMapClasses();
-
   return features[0];
 }
